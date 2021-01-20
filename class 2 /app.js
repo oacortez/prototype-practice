0863d82c1b69a126dc896ffaae9e03956b46000a
Oscar Cortez <oscarcortez@Oscars-MacBook-Pro.local>
2021-01-19 19:43:47 -0700

    added a basic server

---
+++ class 2 /app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const server = http.createServer((req,res) => {
+    res.writeHead(200,{"Content-Type": "tesct/plain" })
+    res.write("yerrrr its me ")
+    res.end();
+
+})
+
+server.listen(3000);
